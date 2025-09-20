659bb859ffda04d86ee4455f980aaa41ee3bfe5b
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2025-09-20 10:24:13 +0530

    AllComponents.tsx

--- src/AllComponents.tsx
+++ src/AllComponents.tsx
@@ -551,7 +551,9 @@ export const LanguageSelector = () => {
     { code: 'en' as Language, name: 'English', flag: '🇬🇧' },
     { code: 'od' as Language, name: 'ଓଡ଼ିଆ', flag: '🇮🇳' },
     { code: 'te' as Language, name: 'తెలుగు', flag: '🇮🇳' },
-    { code: 'bn' as Language, name: 'বাংলা', flag: '🇮🇳' }
+    { code: 'bn' as Language, name: 'বাংলা', flag: '🇮🇳' },
+    { code: 'bn' as Language, name: 'বাংলা', flag: '🇮🇳' }, { code: 'ta' as Language, name: 'தமிழ்', flag: '🇮🇳' }, { code: 'ml' as Language, name: 'മലയാളം', flag: '🇮🇳' }, { code: 'kn' as Language, name: 'ಕನ್ನಡ', flag: '🇮🇳' }, { code: 'gu' as Language, name: 'ગુજરાતી', flag: '🇮🇳' }, { code: 'mr' as Language, name: 'मराठी', flag: '🇮🇳' }, { code: 'pa' as Language, name: 'ਪੰਜਾਬੀ', flag: '🇮🇳' }, { code: 'as' as Language, name: 'অসমীয়া', flag: '🇮🇳' },
+    
   ];
 
   return (
